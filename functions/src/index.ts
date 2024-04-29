0eface8fef5de5923d92113b13e433fb9840999d
y-ohgi <ohgi.211@gmail.com>
2024-04-29 14:44:00 +0900

    add: event listener function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,23 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import * as admin from "firebase-admin";
 import * as logger from "firebase-functions/logger";
+import {setGlobalOptions} from "firebase-functions/v2";
+import {onValueWritten} from "firebase-functions/v2/database";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// MEMO:
+//   Realtime Databaseと同じリージョンで実行したいため、asia-southeast1を指定。
+setGlobalOptions({
+  region: "asia-southeast1",
+});
+
+// MEMO:
+//   "/users/{uid}" 配下で発生したイベントをリッスン
+const onUserWritten = onValueWritten("users/{uid}", (event) => {
+  logger.log(event);
+});
+
+export = {onUserWritten}
